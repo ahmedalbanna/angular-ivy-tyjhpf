b6d9581a8714065f5d1281de9f241323d4d0eae6
ahmedalbanna <ahmedalbanna966@gmail.com>
2022-05-05 03:01:16 +0300

    3 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { TransportationService } from './transportation.service';
+import { Car } from './car';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,20 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  cars: Car[];
+make: string;
+model: string;
+miles: number;
+
+  constructor (private transportationService: TransportationService) {
+    this.cars = this.transportationService.getCars();
+  }
+
+  addCar(){
+    const newCar: Car = {make: this.make, model: this.model, miles: this.miles };
+    this.transportationService.addCar(newCar);
+  
 }
+}
+
